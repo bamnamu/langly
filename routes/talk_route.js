24d8e3be8df5d16840fa5da24d026b2451880f8d
bamnamu <taotrpmar@gmail.com>
2025-01-23 08:33:10 +0000

    last commit

--- routes/talk_route.js
+++ routes/talk_route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { authenticate } = require('../middlewares/jwt_token');
 const { talk_process } = require('../controllers/talk_controller');
 const upload = require('../config/multer');
 
-router.post('/talk_process', upload.single('audio'), talk_process);
+router.post('/talk_process', authenticate, upload.single('audio'), talk_process);
 module.exports = router;
